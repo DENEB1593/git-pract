ee56b3016d39d2ccab1b4e860e581f25f94955dc
deneb1593 <deneb1593@gmail.com>
2021-10-15 16:44:30 +0900

    init

---
+++ src/test/java/com/example/gitpract/GitPractApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitpract;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitPractApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
